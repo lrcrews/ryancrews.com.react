d623b2e3f1df5abbc597d912574c5afe53cf793f
ryan <lrcrews@gmail.com>
2019-07-28 12:46:42 -0700

    create sass mixins, setup some default classes, set fonts

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -8,6 +8,18 @@ const About: React.FC = () => {
     return (
         <div id="about-page">
             <h1>About Page</h1>
+            <h2>the page of aboutness</h2>
+            <h3 className="font-section-header">
+                You may be wondering, "How does he come up with all these page designs?"
+            </h3>
+            <p>
+                Some paragraph content here. I'm merely an engineer who happens to enjoy design, so when I see a design
+                I like I want to see if I can recreate it (or something similar). For instance, this page is based on
+                the level "map" screen of a fantastic came called{' '}
+                <a href="https://www.dots.co/twodots/" target="_blank">
+                    Two Dots
+                </a>
+            </p>
             <p>
                 <Link to="/disclosure">visit disclosure page</Link>
             </p>
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 
 const Home: React.FC = () => {
-    return <h1>Home Page</h1>;
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <h2>the page of home</h2>
+            <h3 className="font-section-header">
+                You may be wondering, "How does he come up with all these page designs?"
+            </h3>
+            <p>
+                Some paragraph content here. I'm merely an engineer who happens to enjoy design, so when I see a design
+                I like I want to see if I can recreate it (or something similar). For instance, this page is based on
+                the level "map" screen of a fantastic came called{' '}
+                <a href="https://www.dots.co/twodots/" target="_blank">
+                    Two Dots
+                </a>
+            </p>
+        </div>
+    );
 };
 export default Home;
