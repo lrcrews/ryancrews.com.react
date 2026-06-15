6faf36acb86227a2a1d3dc1d2fb85edd4b50e4e3
ryan <lrcrews@gmail.com>
2026-06-15 14:50:56 -0700

    move link for consistency

--- src/screens/food/RecipeWrapper.tsx
+++ src/screens/food/RecipeWrapper.tsx
@@ -21,6 +21,9 @@ function RecipeWrapper(props: RecipeWrapperProps) {
   return (
     <PageWrapper>
       <main className="recipe-screen">
+        <p>
+          👈 <Link to={FOOD_HOME_PATH}>Back to recipes</Link>
+        </p>
         <h1>{title}</h1>
         <div className="content">
           <div className="ingrdients-wrapper">
@@ -40,9 +43,6 @@ function RecipeWrapper(props: RecipeWrapperProps) {
             </a>
           </p>
         )}
-        <p>
-          👈 <Link to={FOOD_HOME_PATH}>Back to recipes</Link>
-        </p>
       </main>
     </PageWrapper>
   );
